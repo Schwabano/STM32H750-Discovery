ce1a203c68b9c8eb146db0b4b1b5aec9d54a14a7
Schwabano <tim.zierer@gmail.com>
2025-06-26 14:28:55 -0600

    Initial commit

---
+++ Bootloader/Project/GCC/devenv.cmd
@@ -0,0 +1,40 @@
+@echo off
+
+set Title=Embedded Wizard Build Environment for %PlatformName% V14.00.00.00
+title %Title%
+
+echo *******************************************************************************
+echo * Welcome to %Title%
+echo *******************************************************************************
+echo.
+
+IF DEFINED BuildInitialized GOTO run
+
+GOTO setpaths
+
+:setpaths
+
+:: set the path to make and its tools
+set PATH=%~dp0\..\..\..\ThirdParty\Make;%PATH%
+
+:: compiler path
+set PATH=%~dp0\..\..\..\ThirdParty\gcc-arm-none-eabi\bin;%PATH%
+
+set BuildInitialized=1
+
+:run
+
+echo.
+echo *******************************************************************************
+echo *
+echo * Getting started with Embedded Wizard and %PlatformName% board:
+echo *
+echo * https://doc.embedded-wizard.de/%GettingStartedLink%
+echo *
+echo *******************************************************************************
+echo.
+echo.
+echo Entering bootloader project
+echo.
+
+cd Bootloader\Project\GCC
---
+++ StartGccBuildEnvironment_Bootloader.bat
@@ -0,0 +1,8 @@
+@echo off
+
+:: *************************************************************************************************
+:: Please do NOT change the following settings
+:: *************************************************************************************************
+set PlatformName=STM32H750-Discovery
+set GettingStartedLink=getting-started-stm32h750-discovery
+cmd /K Bootloader\Project\GCC\devenv.cmd
